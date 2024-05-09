d9dd0e149cf17e104f4f99dd072611b85b7038a7
joshu <joshu@REEDPC>
2024-05-08 20:11:59 -0500

    Add project files.

---
+++ ConsoleApp1.1a/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+string myName;
+
+Console.WriteLine("Please enter your name:");
+myName = Console.ReadLine();
+
+Console.WriteLine("My name is " +  myName);
+
+String myAge;
+
+Console.WriteLine("Please enter your age:");
+myAge = Console.ReadLine(); 
+
+Console.WriteLine("My age is " + myAge);
+
+string myAddress;
+Console.WriteLine("Please add your address");
+
+myAddress = Console.ReadLine();
+Console.WriteLine("My address is " + myAddress);
+
+
+
+Console.ReadLine();
+
